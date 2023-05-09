da9040935bfbdcda1359b1f7b7bf4155e5d2927d
tgorsk15 <tgbusiness463@gmail.com>
2023-05-08 19:09:38 -0500

    Create mathmatic functions and empty object
    These functions will hopefully avoid making the other functions too messy by removing some of the
    mathmatic work. The empty object created will serve to store num1, num2, and the operation
    being used so that it can be used

---
+++ script.js
@@ -0,0 +1,30 @@
+// create an object that will account for the currentvalue, operator, 
+//and additional value
+const numStorage = {};
+
+
+//operate function
+// once two numbers areinputed and produce a result, set num1 
+// equal to the result, and reset the operation
+
+
+
+
+
+
+// arithmatic functions
+function addition (num1, num2) {
+    return num1 + num2;
+};
+
+function subtraction (num1, num2) {
+    return num1 - num2;
+};
+
+function multiplication (num1, num2) {
+    return num1 * num2;
+};
+
+function division (num1, num2) {
+    return num1 / num2;
+};
